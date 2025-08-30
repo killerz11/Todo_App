50c3981b659f8a800a1a397368deef4ccb7da8b2
atharva <atharvmutkule19@gmail.com>
2025-08-30 13:04:16 +0530

    Implemented the delete and edit logic

--- Todo_App/frontend/src/App.jsx
+++ Todo_App/frontend/src/App.jsx
@@ -7,6 +7,22 @@ function App() {
   ])
   const [newTodo, setnewTodo] = useState("");
 
+  const[editingId, setEditingId] = useState(null);
+  const[editText, setEditText] = useState("");
+
+  const startEdit = (todo) =>{
+    setEditingId(todo.id);
+    setEditText(todo.text);
+  }
+
+  const saveEdit = (id) => {
+    setTodos(todos.map(todo => 
+      todo.id === id?{...todo, text: editText}: todo
+    ));
+
+    setEditingId(null);
+  }
+
   const addTodo = () => {
     if(!newTodo.trim()) return;
     const todo = {
@@ -23,6 +39,10 @@ function App() {
     setTodos(todos.map( todo => 
       todo.id == id ? {...todo, completed: !todo.completed}: todo
     ))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
   }
 
   return (
@@ -42,6 +62,19 @@ function App() {
           <li key={todo.id}>
             {todo.text} - {todo.completed ? 'completed' : 'not-completed'} - {todo.createdAt.toLocaleDateString()}
             <button onClick={() => toggleTodos(todo.id)}>Status</button>
+            <button onClick={() => startEdit(todo)}>Edit</button>
+            <button onClick={() => deleteTodo(todo.id)}>delete</button>
+
+            {editingId === todo.id && (
+              <div>
+                <input type='text'
+                       value={editText}
+                       onChange={(e) => setEditText(e.target.value)}
+                       />
+                <button onClick={() => saveEdit(todo.id)}>Save</button>  
+                <button onClick={() => setEditingId(null)}>Cancel</button>     
+              </div>
+            )}
           </li>
         ))}
       </ol>
